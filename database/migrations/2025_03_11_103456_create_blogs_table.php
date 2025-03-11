b695d80ea1951bc98150e30614eaffb540fe5a39
AminAhadian <amin.ahadiyan.work@gmial.com>
2025-03-11 14:09:33 +0330

    blogs model and migration added

---
+++ database/migrations/2025_03_11_103456_create_blogs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('blog_category_id')->constrained('blog_categories')->cascadeOnDelete();
+            $table->foreignId('author_id')->constrained('users')->cascadeOnDelete();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content');
+            $table->string('cover_image_url')->nullable();
+            $table->string('status')->default('Draft');
+            $table->timestamp('published_at')->nullable();
+            $table->boolean('active')->default(true);
+            $table->boolean('featured')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
